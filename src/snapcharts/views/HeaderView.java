da1c8070f43554108dc499e46c7f6817b1221808
Jeff Martin <jeff@reportmill.com>
2020-12-31 12:28:15 -0600

    Some improvements.

--- src/snapcharts/views/HeaderView.java
+++ src/snapcharts/views/HeaderView.java
@@ -1,8 +1,10 @@
 package snapcharts.views;
+import snap.geom.Insets;
 import snap.geom.Pos;
 import snap.gfx.Color;
 import snap.gfx.Font;
 import snap.view.ColView;
+import snap.view.ScaleBox;
 import snap.view.StringView;
 import snapcharts.model.Chart;
 import snapcharts.model.Header;
@@ -18,6 +20,17 @@ public class HeaderView<T extends Header> extends ChartPartView<T> {
     // The subtitle
     private StringView  _subtitleView;
 
+    // The ScaleBox in case header doesn't fit
+    private ScaleBox  _scaleBox;
+
+    // The ColView to hold children in ScaleBox
+    private ColView  _colView;
+
+    // Constants
+    private final Font DEFAULT_TITLE_FONT = Font.Arial14.getBold().deriveFont(20);
+    private final Font DEFAULT_SUBTITLE_FONT = Font.Arial12.getBold();
+    private final Color DEFAULT_SUBTITLE_COLOR = Color.GRAY;
+
     /**
      * Constructor.
      */
@@ -29,18 +42,24 @@ public class HeaderView<T extends Header> extends ChartPartView<T> {
         setMargin(0, 0, 8, 0);
         setClipToBounds(true);
 
+        // Create ScaleBox, ColView
+        _colView = new ColView();
+        _colView.setAlign(Pos.TOP_CENTER);
+        _scaleBox = new ScaleBox(_colView);
+        _scaleBox.setKeepAspect(true);
+        _scaleBox.setAlign(Pos.BOTTOM_CENTER);
+        addChild(_scaleBox);
+
         // Create configure TitleView
         _titleView = new StringView();
-        _titleView.setFont(Font.Arial14.getBold().deriveFont(20));
-        _titleView.setShrinkToFit(true);
-        addChild(_titleView);
+        _titleView.setFont(DEFAULT_TITLE_FONT);
+        _colView.addChild(_titleView);
 
         // Create configure SubtitleView
         _subtitleView = new StringView();
-        _subtitleView.setTextFill(Color.GRAY);
-        _subtitleView.setFont(Font.Arial12.getBold());
-        _subtitleView.setShrinkToFit(true);
-        addChild(_subtitleView);
+        _subtitleView.setFont(DEFAULT_SUBTITLE_FONT);
+        _subtitleView.setTextFill(DEFAULT_SUBTITLE_COLOR);
+        _colView.addChild(_subtitleView);
     }
 
     /**
@@ -78,7 +97,8 @@ public class HeaderView<T extends Header> extends ChartPartView<T> {
     @Override
     protected double getPrefWidthImpl(double aH)
     {
-        return ColView.getPrefWidth(this, aH);
+        //return ColView.getPrefWidth(this, aH);
+        return _scaleBox.getPrefWidth(aH);
     }
 
     /**
@@ -87,7 +107,8 @@ public class HeaderView<T extends Header> extends ChartPartView<T> {
     @Override
     protected double getPrefHeightImpl(double aW)
     {
-        return ColView.getPrefHeight(this, aW);
+        //return ColView.getPrefHeight(this, aW);
+        return _scaleBox.getPrefHeight(aW);
     }
 
     /**
@@ -96,6 +117,12 @@ public class HeaderView<T extends Header> extends ChartPartView<T> {
     @Override
     protected void layoutImpl()
     {
-        ColView.layout(this, false);
+        //ColView.layout(this, false);
+        Insets ins = getInsetsAll();
+        double areaX = ins.left;
+        double areaY = ins.top;
+        double areaW = getWidth() - ins.getWidth();
+        double areaH = getHeight() - ins.getHeight();
+        _scaleBox.setBounds(areaX, areaY, areaW, areaH);
     }
 }
