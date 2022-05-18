15d660242177eb962d1bdf02b75ca02b2263820e
Jeff Martin <jeff@reportmill.com>
2022-05-18 14:19:56 -0500

    Contour3DDataArea: Add createTriangleArray() and change createAxisBoxShape() to use Contour3DDataArea: Modify createTriangleArray() to add raw points and triangle index array Contour3DDataArea: Modify getDataPointForLocalXY() to use HitDetector to find 3D hit point Contour3DDataArea: Add getLocalXYForDataPoint() to handle 3D data point HitDetector: Add support for VertexArrayShape and VertexArray with no index HitDetector: Add support for multiple hits and single-sided triangles

--- src/snapcharts/viewx/Contour3DDataArea.java
+++ src/snapcharts/viewx/Contour3DDataArea.java
@@ -2,10 +2,9 @@
  * Copyright (c) 2010, ReportMill Software. All rights reserved.
  */
 package snapcharts.viewx;
+import snap.geom.Point;
 import snap.gfx.Color;
-import snap.gfx3d.VertexArray;
-import snap.gfx3d.VertexArrayShape;
-import snap.util.MathUtils;
+import snap.gfx3d.*;
 import snapcharts.data.DataSet;
 import snapcharts.model.Trace;
 import snapcharts.model.TracePoint;
@@ -16,6 +15,9 @@ import snapcharts.view.ChartHelper;
  * A DataArea3D subclass to display the contents of Contour3D chart.
  */
 public class Contour3DDataArea extends DataArea3D {
+
+    // The Triangle VertexArray
+    private VertexArray  _triangleArray;
 
     /**
      * Constructor.
@@ -31,85 +33,68 @@ public class Contour3DDataArea extends DataArea3D {
     @Override
     protected AxisBoxShape createAxisBoxShape()
     {
-        // Do normal version
+        // Do normal version to get AxisBoxShape
         AxisBoxShape axisBoxShape = super.createAxisBoxShape();
 
-        // Get axis box size
-        double prefW = getAxisBoxPrefWidth();
-        double prefH = getAxisBoxPrefHeight();
-        double prefD = getAxisBoxPrefDepth();
+        // Get TriangleArray, create/add to VertexArrayShape
+        _triangleArray = createTriangleArray();
+        VertexArrayShape triangleArrayShape = new VertexArrayShape(_triangleArray);
 
-        // Get dataset info
-        DataSet dataSet = getStagedData();
-        double minX = dataSet.getMinX();
-        double maxX = dataSet.getMaxX();
-        double minY = dataSet.getMinY();
-        double maxY = dataSet.getMaxY();
-        double minZ = dataSet.getMinZ();
-        double maxZ = dataSet.getMaxZ();
+        // Add TriangleArrayShape to AxisBoxShape, return
+        axisBoxShape.addChild(triangleArrayShape);
+        return axisBoxShape;
+    }
 
+    /**
+     * Override to add contour chart.
+     */
+    protected VertexArray createTriangleArray()
+    {
         // Get contour mesh and triangles
+        DataSet dataSet = getStagedData();
         Mesh mesh = new Mesh(dataSet);
         Mesh.Triangle[] triangles = mesh.getTriangles();
 
         // Get ContourHelper and ContourCount
         Contour3DChartHelper chartHelper = (Contour3DChartHelper) getChartHelper();
         ContourHelper contourHelper = chartHelper.getContourHelper();
-        int contourCount = contourHelper.getContourCount();
 
-        // Create VertexArrayShape
-        VertexArrayShape vertexArrayShape = new VertexArrayShape();
-        vertexArrayShape.setDoubleSided(true);
-        VertexArray vertexArray = vertexArrayShape.getTriangleArray();
-        axisBoxShape.addChild(vertexArrayShape);
+        // Create VertexArray
+        VertexArray vertexArray = new VertexArray();
+        vertexArray.setDoubleSided(true);
 
         // Iterate over triangles and add shape for each
-        for (Mesh.Triangle triangle : triangles) {
+        int pointCount = mesh.getPointCount();
+        Point3D meshPoint = new Point3D();
+        for (int i = 0; i < pointCount; i++) {
 
-            // Get triangle vertex indexes
-            int v1 = triangle.v1;
-            int v2 = triangle.v2;
-            int v3 = triangle.v3;
+            // Get mesh point in scene coords
+            meshPoint.x = mesh.getX(i);
+            meshPoint.y = mesh.getY(i);
+            double pz = meshPoint.z = mesh.getZ(i);
+            convertDataToScene(meshPoint);
 
-            // Get triangle points
-            double p1x = mesh.getX(v1);
-            double p1y = mesh.getY(v1);
-            double p1z = mesh.getZ(v1);
-            double p2x = mesh.getX(v2);
-            double p2y = mesh.getY(v2);
-            double p2z = mesh.getZ(v2);
-            double p3x = mesh.getX(v3);
-            double p3y = mesh.getY(v3);
-            double p3z = mesh.getZ(v3);
+            // Get vertex point and add (notice we swap Y and Z)
+            vertexArray.addPoint(meshPoint.x, meshPoint.y, meshPoint.z);
 
-            // Get vertex colors
-            Color contourColor1 = contourHelper.getContourColorForZ(p1z);
-            Color contourColor2 = contourHelper.getContourColorForZ(p2z);
-            Color contourColor3 = contourHelper.getContourColorForZ(p3z);
-
-            p1x = MathUtils.mapValueForRanges(p1x, minX, maxX, 0, prefW);
-            p1y = MathUtils.mapValueForRanges(p1y, minY, maxY, 0, prefD);
-            p1z = MathUtils.mapValueForRanges(p1z, minZ, maxZ, 0, prefH);
-            p2x = MathUtils.mapValueForRanges(p2x, minX, maxX, 0, prefW);
-            p2y = MathUtils.mapValueForRanges(p2y, minY, maxY, 0, prefD);
-            p2z = MathUtils.mapValueForRanges(p2z, minZ, maxZ, 0, prefH);
-            p3x = MathUtils.mapValueForRanges(p3x, minX, maxX, 0, prefW);
-            p3y = MathUtils.mapValueForRanges(p3y, minY, maxY, 0, prefD);
-            p3z = MathUtils.mapValueForRanges(p3z, minZ, maxZ, 0, prefH);
-
-            // Create/config path for triangle
-            vertexArray.addPoint(p1x, p1z, p1y);
-            vertexArray.addPoint(p2x, p2z, p2y);
-            vertexArray.addPoint(p3x, p3z, p3y);
-
-            // Add triangle vertex colors
+            // Add vertex color and add
+            Color contourColor1 = contourHelper.getContourColorForZ(pz);
             vertexArray.addColor(contourColor1);
-            vertexArray.addColor(contourColor2);
-            vertexArray.addColor(contourColor3);
         }
 
+        // Create indexArray for triangles and set
+        int indexCount = triangles.length * 3;
+        int[] indexArray = new int[indexCount];
+        int index = 0;
+        for (Mesh.Triangle triangle : triangles) {
+            indexArray[index++] = triangle.v1;
+            indexArray[index++] = triangle.v2;
+            indexArray[index++] = triangle.v3;
+        }
+        vertexArray.setIndexArray(indexArray);
+
         // Return
-        return axisBoxShape;
+        return vertexArray;
     }
 
     /**
@@ -118,8 +103,58 @@ public class Contour3DDataArea extends DataArea3D {
     @Override
     public TracePoint getDataPointForLocalXY(double aX, double aY)
     {
+        // If point above cube view, just return
         if (aX > getWidth() - 100 && aY < 100)
             return null;
-        return super.getDataPointForLocalXY(aX, aY);
+
+        // Get ray from camera origin to View XY
+        Camera camera = _camera;
+        Point3D rayOrigin = new Point3D();
+        Vector3D rayDir = new Vector3D();
+        camera.getRayToViewPoint(aX, aY, rayOrigin, rayDir);
+
+        // Do hit detection (just return null if missed)
+        HitDetector hitDetector = new HitDetector();
+        boolean isHit = hitDetector.isRayHitShape(rayOrigin, rayDir, camera.getScene());
+        if (!isHit) {
+            System.out.println("Missed");
+            return null;
+        }
+
+        // Get hit shape
+        Shape3D hitShape = hitDetector.getHitShape();
+        String hitShapeName = hitShape.getName(); if (hitShapeName == null) hitShapeName = "(no-name)";
+        //Point3D hitPoint = hitDetector.getHitPoint();
+        //System.out.println("Hit Shape: " + hitShapeName + " at Point: " + hitPoint.x + ", " + hitPoint.y + ", " + hitPoint.z);
+
+        // If hit point isn't mesh, just return null (probably hit axis box)
+        VertexArray hitTriangleArray = hitDetector.getHitTriangleArray();
+        if (hitTriangleArray != _triangleArray)
+            return null;
+
+        // Get closest mesh point and return
+        Trace trace = getTrace();
+        int pointIndex = hitDetector.getHitVertexIndex();
+        if (pointIndex >= trace.getPointCount()) {
+            System.err.println("Contour3DDataArea.getDataPointForLocalXY: hit point index outside trace bounds! " + pointIndex);
+            return null;
+        }
+
+        // Return point
+        return trace.getPoint(pointIndex);
+    }
+
+    /**
+     * Override to handle 3D data point.
+     */
+    @Override
+    public Point getLocalXYForDataPoint(TracePoint aDP)
+    {
+        // If null, just return
+        if (aDP == null) return null;
+
+        // Get point in view coords and return as 2D point
+        Point3D point = convertDataToView(aDP.getX(), aDP.getY(), aDP.getZ());
+        return new Point(point.x, point.y);
     }
 }
