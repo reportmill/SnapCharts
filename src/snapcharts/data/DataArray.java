f9409602df9c6282c8653b66925dbc72ea8580d7
Jeff Martin <jeff@reportmill.com>
2022-06-02 12:19:33 -0500

    DataArray: First stab at generic array of float, double, String

---
+++ src/snapcharts/data/DataArray.java
@@ -0,0 +1,299 @@
+/*
+ * Copyright (c) 2010, ReportMill Software. All rights reserved.
+ */
+package snapcharts.data;
+import snap.util.SnapUtils;
+import java.util.Arrays;
+import java.util.Objects;
+
+/**
+ * This class manages an array of primitive data values (float, double, String).
+ */
+public abstract class DataArray implements Cloneable {
+
+    // The name
+    private String  _name;
+
+    // The array length
+    protected int  _length;
+
+    // The float array
+    protected float[]  _floatArray;
+
+    // The double array
+    protected double[]  _doubleArray;
+
+    // The String array
+    protected String[]  _stringArray;
+
+    /**
+     * Returns the name.
+     */
+    public String getName()  { return _name; }
+
+    /**
+     * Sets the name.
+     */
+    public void setName(String aName)
+    {
+        if (Objects.equals(aName, _name)) return;
+        _name = aName;
+    }
+
+    /**
+     * Returns the component type.
+     */
+    public abstract Class getComponentType();
+
+    /**
+     * Returns the length.
+     */
+    public final int getLength()  { return _length; }
+
+    /**
+     * Returns the float value at index.
+     */
+    public float getFloat(int anIndex)
+    {
+        return (float) getDouble(anIndex);
+    }
+
+    /**
+     * Sets the float value at index.
+     */
+    public void setFloat(float aValue, int anIndex)
+    {
+        setDouble(aValue, anIndex);
+    }
+
+    /**
+     * Adds the float value at end.
+     */
+    public void addFloat(float aValue)
+    {
+        addFloat(aValue, getLength());
+    }
+
+    /**
+     * Adds the float value at index.
+     */
+    public void addFloat(float aValue, int anIndex)
+    {
+        addDouble(aValue, anIndex);
+    }
+
+    /**
+     * Returns the double value at index.
+     */
+    public abstract double getDouble(int anIndex);
+
+    /**
+     * Sets the double value at index.
+     */
+    public abstract void setDouble(double aValue, int anIndex);
+
+    /**
+     * Adds the double value at end.
+     */
+    public void addDouble(double aValue)
+    {
+        addDouble(aValue, getLength());
+    }
+
+    /**
+     * Adds the double value at index.
+     */
+    public abstract void addDouble(double aValue, int anIndex);
+
+    /**
+     * Returns the String value at index.
+     */
+    public String getString(int anIndex)
+    {
+        Object value = getValue(anIndex);
+        return SnapUtils.stringValue(value);
+    }
+
+    /**
+     * Sets the String value at index.
+     */
+    public abstract void setString(String aValue, int anIndex);
+
+    /**
+     * Returns the Object value at index.
+     */
+    public abstract Object getValue(int anIndex);
+
+    /**
+     * Returns the float array.
+     */
+    public float[] getFloatArray()
+    {
+        // If already set, just return
+        if (_floatArray != null) return _floatArray;
+
+        // Create/load array
+        double[] doubleArray = getDoubleArray();
+        int length = doubleArray.length;
+        float[] floatArray = new float[length];
+        for (int i = 0; i < length; i++)
+            floatArray[i] = (float) doubleArray[i];
+
+        // Set and return
+        return _floatArray = floatArray;
+    }
+
+    /**
+     * Returns the double array.
+     */
+    public double[] getDoubleArray()
+    {
+        // If already set, just return
+        if (_doubleArray != null) return _doubleArray;
+
+        // Create/load array
+        float[] floatArray = getFloatArray();
+        int length = floatArray.length;
+        double[] doubleArray = new double[length];
+        for (int i = 0; i < length; i++)
+            doubleArray[i] = floatArray[i];
+
+        // Set and return
+        return _doubleArray = doubleArray;
+    }
+
+    /**
+     * Returns the String array.
+     */
+    public String[] getStringArray()
+    {
+        // If already set, just return
+        if (_stringArray != null) return _stringArray;
+
+        // Create/load array
+        int length = getLength();
+        String[] stringArray = new String[length];
+        for (int i = 0; i < length; i++)
+            stringArray[i] = getString(i);
+
+        // Set and return
+        return _stringArray = stringArray;
+    }
+
+    /**
+     * Standard clone implementation.
+     */
+    @Override
+    protected DataArray clone()
+    {
+        // Do normal version
+        DataArray clone;
+        try { clone = (DataArray) super.clone(); }
+        catch (Exception e) { throw new RuntimeException(e); }
+
+        // Clone arrays
+        if (_floatArray != null) clone._floatArray = _floatArray.clone();
+        if (_doubleArray != null) clone._doubleArray = _doubleArray.clone();
+        if (_stringArray != null) clone._stringArray = _stringArray.clone();
+
+        // Return
+        return clone;
+    }
+
+    /**
+     * This DataArray subclass uses doubles.
+     */
+    public static class Double extends DataArray {
+
+        /**
+         * Constructor.
+         */
+        public Double()
+        {
+            _doubleArray = new double[10];
+        }
+
+        /**
+         * Constructor.
+         */
+        public Double(double[] doubleArray)
+        {
+            _doubleArray = doubleArray.clone();
+        }
+
+        /**
+         * Returns the component type.
+         */
+        public Class getComponentType()  { return double.class; }
+
+        /**
+         * Returns the double value at index.
+         */
+        public final double getDouble(int anIndex)
+        {
+            return _doubleArray[anIndex];
+        }
+
+        /**
+         * Sets the double value at index.
+         */
+        public final void setDouble(double aValue, int anIndex)
+        {
+            // Set value
+            _doubleArray[anIndex] = aValue;
+
+            // Clear caches
+            _floatArray = null;
+            _stringArray = null;
+        }
+
+        /**
+         * Adds the double value at index.
+         */
+        public void addDouble(double aValue, int anIndex)
+        {
+            // Expand components array if needed
+            if (_length == _doubleArray.length)
+                _doubleArray = Arrays.copyOf(_doubleArray, Math.max(_doubleArray.length * 2, 20));
+
+            // If index is inside current length, shift existing elements over
+            if (anIndex < _length)
+                System.arraycopy(_doubleArray, anIndex, _doubleArray, anIndex + 1, _length - anIndex);
+
+            // Set value and increment length
+            _doubleArray[anIndex] = aValue;
+            _length++;
+
+            // Clear caches
+            _floatArray = null;
+            _stringArray = null;
+        }
+
+        /**
+         * Returns the Object value at index.
+         */
+        public java.lang.Double getValue(int anIndex)
+        {
+            return _doubleArray[anIndex];
+        }
+
+        /**
+         * Sets the String value at index.
+         */
+        public void setString(String aValue, int anIndex)
+        {
+            double value = SnapUtils.doubleValue(aValue);
+            setDouble(value, anIndex);
+        }
+
+        /**
+         * Override to trim array.
+         */
+        public double[] getDoubleArray()
+        {
+            if (_length != _doubleArray.length)
+                _doubleArray = Arrays.copyOf(_doubleArray, _length);
+            return _doubleArray;
+        }
+    }
+}
