603d848c68769d74de462634ad39e05abd925501
Jeff Martin <jeff@reportmill.com>
2022-08-11 15:35:12 -0500

    Processor, SubProc, ...: Removed old KeyChain based notebook processor

--- src/snapcharts/notebook/Processor.java
+++ src/snapcharts/notebook/Processor.java
@@ -2,46 +2,37 @@
  * Copyright (c) 2010, ReportMill Software. All rights reserved.
  */
 package snapcharts.notebook;
-import snap.util.KeyChain;
-import snap.util.SnapUtils;
-import java.util.HashMap;
-import java.util.Map;
+import javakit.reflect.Resolver;
+import javakit.shell.JavaShell;
+import java.util.List;
 
 /**
- * This class processes request expressions.
+ * This Processor implementation executes Java.
  */
-public class Processor implements KeyChain.FunctionHandler {
+public class Processor {
 
     // The Notebook this processor works with
     private Notebook  _notebook;
 
-    // A map holding values
-    protected Map<String,Double>  _variables = new HashMap<>();
-
-    // The Math Processor
-    private SubProcMath  _mathProcessor;
-
-    // The Data Processor
-    protected SubProcData  _dataProcessor;
-
-    // The Plot Processor
-    protected SubProcPlot  _plotProcessor;
-
-    // The UI Processor
-    private SubProcUI _uiProcessor;
+    // The JavaShell
+    private JavaShell  _javaShell;
 
     /**
      * Constructor.
      */
     public Processor(Notebook aNotebook)
     {
+        // Set Notebook
         _notebook = aNotebook;
 
-        // Create sub processors
-        _mathProcessor = new SubProcMath(this);
-        _dataProcessor = new SubProcData(this);
-        _plotProcessor = new SubProcPlot(this);
-        _uiProcessor = new SubProcUI(this);
+        // Create JavaShell
+        _javaShell = new JavaShell();
+        _javaShell.setREPLClassName(ChartsREPL.class.getName());
+        _javaShell.addImport("snapcharts.data.*");
+
+        // Link up StaticResolver for TeaVM
+        if (Resolver.isTeaVM)
+            javakit.reflect.StaticResolver.shared()._next = new snapcharts.notebook.StaticResolver();
     }
 
     /**
@@ -50,127 +41,55 @@ public class Processor implements KeyChain.FunctionHandler {
     public Notebook getNotebook()  { return _notebook; }
 
     /**
-     * Called when notebook change requires a full reset.
+     * Returns the JavaText.
      */
-    public void resetAll()  { }
+    public String getJavaText()
+    {
+        String javaText = "";
+        List<Request> requests = getNotebook().getRequests();
+        for (Request request : requests) {
+            javaText += request.getText();
+            if (!javaText.endsWith("\n"))
+                javaText += '\n';
+        }
+        return javaText;
+    }
+
+    /**
+     * Resets all.
+     */
+    public void resetAll()
+    {
+        String javaText = getJavaText();
+
+        _javaShell.runJavaCode(javaText);
+    }
 
     /**
      * Returns the snippet out for a snippet.
      */
     public Response createResponseForRequest(Request aRequest)
     {
-        // Get Request.Text as String and KeyChain
-        String text = aRequest.getText();
-        KeyChain keyChain = KeyChain.getKeyChain(text);
+        // Get line values
+        Object[] lineValues = _javaShell.getLineValues();
 
-        // Process keyChain
-        Object responseValue = getValue(this, keyChain);
+        // Get Request.StartLine as String and KeyChain
+        int lineStart = aRequest.getLineStart();
+        int lineEnd = aRequest.getLineEnd();
+        Object lineValue = null;
+        for (int i = lineEnd - 1; i >= lineStart; i--) {
+            if (lineValues[i] != null) {
+                lineValue = lineValues[i];
+                if (!(lineValue instanceof String) || ((String) lineValue).length() > 0)
+                    break;
+            }
+        }
 
         // Create Response and set value
         Response response = new Response();
-        response.setValue(responseValue);
+        response.setValue(lineValue);
 
         // Return
         return response;
-    }
-
-    /**
-     * Returns a value for given object and keychain.
-     */
-    public Object getValue(Object anObj, KeyChain aKC)
-    {
-        // Register as FunctionHandler
-        KeyChain.setFunctionHandler(this);
-
-        // Get value
-        Object value = KeyChain.getValue(anObj, aKC);
-
-        // Return
-        return value;
-    }
-
-    /**
-     * Returns the string value of a keychain.
-     */
-    public String getStringValue(Object anObj, KeyChain aKC)
-    {
-        // Get value
-        Object value;
-        try { value = getValue(anObj, aKC); }
-
-        // If exception, set as response
-        catch (Exception e) {
-            value = e.toString();
-        }
-
-        // Convert to string and return
-        String str = value != null ? SnapUtils.stringValue(value) : "null";
-        return str;
-    }
-
-    /**
-     * Handles FunctionCall KeyChains.
-     */
-    @Override
-    public Object getValueFunctionCall(Object aRoot, Object anObj, KeyChain aKeyChain)
-    {
-        try {
-            return getValueFunctionCallImpl(aRoot, anObj, aKeyChain);
-        }
-        catch (Exception e) {
-            return e.toString().replace(e.getClass().getName(), "Error");
-        }
-    }
-
-    /**
-     * Handles FunctionCall KeyChains.
-     */
-    protected Object getValueFunctionCallImpl(Object aRoot, Object anObj, KeyChain aKeyChain)
-    {
-        // Get function name (lowercase) and arg list
-        String functionName = aKeyChain.getChildString(0).toLowerCase();
-        KeyChain argList = aKeyChain.getChildKeyChain(1);
-
-        // Dispatch for function name
-        switch (functionName) {
-
-            // Handle text
-            case "text": return text(anObj, argList);
-        }
-
-        // Try Math Processor
-        Object value = _mathProcessor.getValueFunctionCall(aRoot, anObj, functionName, argList);
-        if (value != null)
-            return value;
-
-        // Try Data Processor
-        value = _dataProcessor.getValueFunctionCall(aRoot, anObj, functionName, argList);
-        if (value != null)
-            return value;
-
-        // Try Plot Processor
-        value = _plotProcessor.getValueFunctionCall(aRoot, anObj, functionName, argList);
-        if (value != null)
-            return value;
-
-        // Try UI Processor
-        value = _uiProcessor.getValueFunctionCall(aRoot, anObj, functionName, argList);
-        if (value != null)
-            return value;
-
-        // Complain and return null since function not found
-        System.err.println("Processor.getValueFunctionCall: Unknown function: " + functionName);
-        return null;
-    }
-
-    /**
-     * Returns the nested keyChain as a string.
-     */
-    public String text(Object anObj, KeyChain aKeyChain)
-    {
-        KeyChain keyChain = aKeyChain.getChildKeyChain(0);
-        Object value = getValue(anObj, keyChain);
-        String str = ProcessorUtils.getStringForValue(value);
-        return str;
     }
 }
