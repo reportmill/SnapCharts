b9f13fdb09df6afecf7416283ff9188e96d96faf
Jeff Martin <jeff@reportmill.com>
2024-11-22 10:47:47 -0600

    TextLineStyle: Changed to be PropObject and renamed prop keys and copy methods

--- src/snapcharts/app/WelcomePanelAnim.java
+++ src/snapcharts/app/WelcomePanelAnim.java
@@ -25,37 +25,37 @@ public class WelcomePanelAnim extends ViewOwner {
         // Configure MainTitleText
         TextArea mainTitleText = getView("MainTitleText", TextArea.class);
         mainTitleText.setDefaultTextStyleString("Font: Arial Black 58; CharSpacing: -1.9;");
-        mainTitleText.setDefaultLineStyle(mainTitleText.getDefaultLineStyle().copyFor(HPos.CENTER));
+        mainTitleText.setDefaultLineStyle(mainTitleText.getDefaultLineStyle().copyForAlign(HPos.CENTER));
         mainTitleText.addChars("SnapCharts");
 
         // Configure MainTitleText2
         TextArea mainTitleText2 = getView("MainTitleText2", TextArea.class);
         mainTitleText2.setRichText(true);
         mainTitleText2.setDefaultTextStyleString("Font: Arial Black 58; CharSpacing: -1.9;");
-        mainTitleText2.setDefaultLineStyle(mainTitleText2.getDefaultLineStyle().copyFor(HPos.CENTER));
+        mainTitleText2.setDefaultLineStyle(mainTitleText2.getDefaultLineStyle().copyForAlign(HPos.CENTER));
         mainTitleText2.addCharsWithStyleString("Snap", "Color: #ff5a5a");
         mainTitleText2.addCharsWithStyleString("Charts", "Color: #bed0ff");
 
         // Configure TagLineText, TagLineText2
         TextArea tagLineTextArea = getView("TagLineText", TextArea.class);
         tagLineTextArea.setDefaultTextStyleString("Font: Arial Bold 14");
-        tagLineTextArea.setDefaultLineStyle(tagLineTextArea.getDefaultLineStyle().copyFor(HPos.CENTER));
+        tagLineTextArea.setDefaultLineStyle(tagLineTextArea.getDefaultLineStyle().copyForAlign(HPos.CENTER));
         tagLineTextArea.addChars("An app for building charts");
         TextArea tagLineTextArea2 = getView("TagLineText2", TextArea.class);
         tagLineTextArea2.setDefaultTextStyleString("Font: Arial Bold 14; Color: #FF");
-        tagLineTextArea2.setDefaultLineStyle(tagLineTextArea2.getDefaultLineStyle().copyFor(HPos.CENTER));
+        tagLineTextArea2.setDefaultLineStyle(tagLineTextArea2.getDefaultLineStyle().copyForAlign(HPos.CENTER));
         tagLineTextArea2.addChars("An app for building charts");
 
         // Configure JVMText
         TextArea jvmText = getView("JVMText", TextArea.class);
         jvmText.setDefaultTextStyleString("Font: Arial Bold 10; Color: #FF");
-        jvmText.setDefaultLineStyle(jvmText.getDefaultLineStyle().copyFor(HPos.CENTER));
+        jvmText.setDefaultLineStyle(jvmText.getDefaultLineStyle().copyForAlign(HPos.CENTER));
         jvmText.addChars("JVM: " + (SnapUtils.isTeaVM ? "TeaVM" : System.getProperty("java.runtime.version")));
 
         // Configure BuildText
         TextArea buildText = getView("BuildText", TextArea.class);
         buildText.setDefaultTextStyleString("Font: Arial Bold 10; Color: #FF");
-        buildText.setDefaultLineStyle(buildText.getDefaultLineStyle().copyFor(HPos.CENTER));
+        buildText.setDefaultLineStyle(buildText.getDefaultLineStyle().copyForAlign(HPos.CENTER));
         buildText.addChars("Build: " + SnapUtils.getBuildInfo());
 
         // Configure anim for hammer and screwdriver
